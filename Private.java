4415945ba73e3b80609261faea59ec2476dca8d0
uldefahmi <fahmiulde@gmail.com>
2020-07-26 00:21:00 +0530

    Add files via upload
    Example of Java private Access specifier

---
+++ Private.java
@@ -0,0 +1,21 @@
+class A{
+ int data=40;
+public void msg(){
+	System.out.println("hello");
+	
+	}	
+	}
+class Private{
+public static void main(String args[]){
+	
+	A obj=new A();
+	System.out.println(obj.data);
+	
+	obj.msg();
+	
+	
+	
+	
+	
+	}	
+	}	
